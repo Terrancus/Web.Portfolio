090e68424bfe57b8e5f5182e1827d641527a096d
Terrance of HK <terrance.of.h.k@gmail.com>
2023-10-07 23:35:21 +0800

    Deployed website

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/Web-Portfolio/" element={<Home />} />
-          <Route path="/Web-Portfolio/projects" element={<Projects />} />
-          <Route path="/Web-Portfolio/project/:id" element={<ProjectDisplay/> } />
-          <Route path="/Web-Portfolio/experience" element={<Experience />} />
+          <Route path="/Web.Portfolio/" element={<Home />} />
+          <Route path="/Web.Portfolio/projects" element={<Projects />} />
+          <Route path="/Web.Portfolio/project/:id" element={<ProjectDisplay/> } />
+          <Route path="/Web.Portfolio/experience" element={<Experience />} />
         </Routes>
         <Footer />
       </Router>
